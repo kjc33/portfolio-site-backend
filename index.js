cc156bd008c827c1062ec407c414b9ea00dbd9a7
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-14 13:30:34 -0700

    Connected to DB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require('express');
+const { testConnection } = require('./db/conn');
+console.log(testConnection);
 
 const PORT = 8080;
 const app = express();
 app.use(express.json());
+
+testConnection();
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
