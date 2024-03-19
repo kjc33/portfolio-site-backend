4c8ccfd88616282303bea782cb9865842e61ed48
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-18 21:49:10 -0700

    Added Error Handling to ENV Route

--- routes/EnvironmentalVariablesRoutes.js
+++ routes/EnvironmentalVariablesRoutes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/api/env", (req, res) => {
-  // Retrieve environment variables from your backend's environment
-  const envVariables = {
-    CONTACT_FORM_ROUTES: process.env.CONTACT_FORM_ROUTES,
-  };
+  try {
+    // Retrieve environment variables from your backend's environment
+    const envVariables = {
+      CONTACT_FORM_ROUTES: process.env.CONTACT_FORM_ROUTES,
+    };
 
-  // Send environment variables as JSON response
-  res.json(envVariables);
+    // Send environment variables as JSON response
+    res.json(envVariables);
+  } catch (error) {
+    // Handle any errors that occur during the retrieval or sending of environment variables
+    console.error("Error fetching environment variables:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
