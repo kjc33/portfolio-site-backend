11d233cfbde9200982eae4f3de75ee4366afe723
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-03-18 21:25:35 -0700

    Updated ENV in Routes

--- routes/EnvironmentalVariablesRoutes.js
+++ routes/EnvironmentalVariablesRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const app = express();
 
 app.get('/api/env', (req, res) => {
   // Retrieve environment variables from your backend's environment
   const envVariables = {
-    BACKEND_API: process.env.CONTACT_FORM_ROUTES
+    CONTACT_FORM_ROUTES: process.env.CONTACT_FORM_ROUTES
   };
 
   // Send environment variables as JSON response
